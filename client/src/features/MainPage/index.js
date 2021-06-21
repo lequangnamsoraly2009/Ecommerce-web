917c5c06576e02ad2907fb8fcdc2ccf78d57c9ef
soraly <lequangnam1617@gmail.com>
2021-06-21 07:46:07 +0700

    upgrade 404

--- client/src/features/MainPage/index.js
+++ client/src/features/MainPage/index.js
@@ -1,20 +1,36 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Products from "./pages/Products";
 import DetailProduct from "./components/detailProduct";
 import Cart from "./pages/Cart";
 import NotFound from "../../components/NotFound";
 import AuthUser from "./pages/Auth";
+import { GlobalState } from "../../GlobalState";
 
 function MainPage() {
+  const state = useContext(GlobalState);
+  const [isLogged] = state.userAPI.isLogged;
   return (
-    <Switch>
-      <Route path="/" exact component={Products} />
-      <Route path="/detail/:id" exact component={DetailProduct} />
-      <Route path="/cart" exact component={Cart} />
-      <Route path="/user"  component={AuthUser} />
-      <Route path="*" exact component={NotFound} />
-    </Switch>
+    <>
+      {!isLogged ? (
+        <>
+          <Route path="/user" component={AuthUser} />
+          <Route path="/" exact component={Products} />
+          <Route path="*" exact component={NotFound} />
+        </>
+      ) : (
+        <>
+          <Switch>
+            <Route path="/" exact component={Products} />
+            <Route path="/detail/:id" exact component={DetailProduct} />
+            <Route path="/cart" exact component={Cart} />
+            <Redirect from="user/login" to="/" />
+            <Redirect from="user/register" to="/" />
+            <Route path="*" exact component={NotFound} />
+          </Switch>
+        </>
+      )}
+    </>
   );
 }
 
